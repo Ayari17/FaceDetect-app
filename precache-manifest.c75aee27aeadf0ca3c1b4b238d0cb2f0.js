0a4df352a240bd3e429ff13a45c98bc80fa6154c
Ayari Ahmed <ayari17@hotmail.fr>
2020-04-21 06:10:16 +0100

    Updates

---
+++ precache-manifest.c75aee27aeadf0ca3c1b4b238d0cb2f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844d0027f63e83703f68d9fa27291b36",
+    "url": "/FaceDetect-app/index.html"
+  },
+  {
+    "revision": "fbf7458cebcde02fbafd",
+    "url": "/FaceDetect-app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "70d973a1a6299dd771af",
+    "url": "/FaceDetect-app/static/css/main.c983a7f2.chunk.css"
+  },
+  {
+    "revision": "fbf7458cebcde02fbafd",
+    "url": "/FaceDetect-app/static/js/2.f2542588.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceDetect-app/static/js/2.f2542588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d973a1a6299dd771af",
+    "url": "/FaceDetect-app/static/js/main.776221ff.chunk.js"
+  },
+  {
+    "revision": "15fa402ada9e52e66bf2",
+    "url": "/FaceDetect-app/static/js/runtime-main.d3725e75.js"
+  }
+]);
